0ff844eb0bf26a55dc93f2ac2885188dedf6096b
shivam3050 <shivamkumar30508209@gmail.com>
2025-07-04 02:09:19 +0530

    css improved for android chrome

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -10,20 +10,20 @@ import Loading from "../utilitiesCompo/loading";
 
 export function Home() {
 
-    useEffect(() => {
+    // useEffect(() => {
 
 
 
-        const setVh = () => {
-            const vh = window.innerHeight * 0.01;
-            document.documentElement.style.setProperty('--vh', `${vh}px`);
-        };
+    //     const setVh = () => {
+    //         const vh = window.innerHeight * 0.01;
+    //         document.documentElement.style.setProperty('--vh', `${vh}px`);
+    //     };
 
-        setVh(); // initial run
-        window.addEventListener('resize', setVh);
+    //     setVh(); // initial run
+    //     window.addEventListener('resize', setVh);
 
-        return () => window.removeEventListener('resize', setVh);
-    }, []);
+    //     return () => window.removeEventListener('resize', setVh);
+    // }, []);
 
 
 
@@ -136,7 +136,7 @@ export function Home() {
             newQueriesSender(socketContainer.current, user, "query-message", "refresh-all-user")
         }
         setToggleSelect(false)
-        if(e.target.value==="logout"){
+        if (e.target.value === "logout") {
             socketContainer.current.close()
             setUser(null)
         }
@@ -157,20 +157,29 @@ export function Home() {
 
         user ? (
             <div className="home dashboard" >
-         
+
                 <header >
                     <button
-                        style={{ visibility: selectedReciever ? "visible" : "hidden" }} onClick={
+                        style={{ visibility: selectedReciever ? "visible" : "hidden", backgroundColor: "transparent" }} onClick={
                             () => { setSelectedReciever(""); setUser_vs_chat_flag(true); setAvailableChats([]) }}>
-                        ←</button>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
+                        </svg></button>
 
-                    <div>{selectedReciever}</div>
+                    <div style={{
+                        fontFamily: "cursive",
+                        color: "var(--professional-blue)",
+                        fontWeight: "bold",
+                        fontSize: "18px",
+                        textDecoration: "underline"
+                    }}
+                    ><i>{selectedReciever}</i></div>
 
-                    <section 
-             
-                    onClick={(e) => { toggleSelectCallback(e) }} id="controls"
+                    <section
+
+                        onClick={(e) => { toggleSelectCallback(e) }} id="controls"
                         className="button"
-style={{ visibility: selectedReciever ? "hidden" : "visible" }}
+                        style={{ visibility: selectedReciever ? "hidden" : "visible" }}
 
                     >
                         <div>
@@ -181,12 +190,35 @@ style={{ visibility: selectedReciever ? "hidden" : "visible" }}
 
 
                         <aside style={{
-                            right: toggleSelect ? "0" : "calc(-3*var(--min-padding) - var(--toggle-select-width) - 10px)",
+                            right: toggleSelect ? "0" : "-100vw",
+
 
                         }}>
-                            <button onClick={(e) => { controlUserCallback(e) }} className="option" value="refresh">Refresh</button>
-                            <button onClick={(e) => { controlUserCallback(e) }} className="option" value="close">Close</button>
-                            <button onClick={(e) => { controlUserCallback(e) }} className="option" value="logout" style={{color:"red"}}>Logout</button>
+                            <button onClick={(e) => { controlUserCallback(e) }} className="option" value="close">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                                </svg>
+                            </button>
+                            <button onClick={(e) => { controlUserCallback(e) }} className="option" value="username"
+                                style={{
+                                    fontFamily: "cursive",
+                                    color: "var(--professional-blue)",
+                                    fontWeight: "bold",
+                                    fontSize: "18px",
+                                    textDecoration: "underline"
+                                }}><i>{user}</i></button>
+                            <button onClick={(e) => { controlUserCallback(e) }} className="option" value="refresh" style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: '10px' }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+                                    <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9" />
+                                    <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z" />
+                                </svg>Users
+                            </button>
+                            <button onClick={(e) => { controlUserCallback(e) }} className="option" value="logout" style={{ display: "flex", alignItems: "center", justifyContent: "center", gap: '10px', color: "red" }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z" />
+                                    <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z" />
+                                </svg> Logout
+                            </button>
                         </aside>
 
 
@@ -195,13 +227,13 @@ style={{ visibility: selectedReciever ? "hidden" : "visible" }}
                 </header>
 
                 {user_vs_chat_flag ? (
-                    <aside className="">
+                    <div>
                         <UserSection
 
                             availableUsers={availableUsers}
                             onUserClick={handleUserClickOnUserSection}
                         />
-                    </aside>
+                    </div>
 
                 ) : (
                     <aside className="user-vs-chat-container">
@@ -210,28 +242,41 @@ style={{ visibility: selectedReciever ? "hidden" : "visible" }}
                             e.preventDefault();
                             newMessageSendHandler(e)
                         }}>
-                            <textarea required
+                            <div>
+                                <textarea required alignItems="center"
 
-                                style={{ resize: "none" }} placeholder={`send to ${selectedReciever}...`} name="message" maxLength="500"></textarea>
-                            <input type="submit" value="➤" />
+                                    style={{ resize: "none" }} placeholder={`Send to ${selectedReciever}...`} name="message" maxLength="500">
+
+                                </textarea>
+                            </div>
+                            <div>
+                                <button type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                                        <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
+                                    </svg>
+                                </button>
+                            </div>
                         </form>
                     </aside>
 
                 )}
 
-                <div style={
+                {/* <div style={
                     {
-                    
-                        display:toggleSelect?"block":"none"
+
+                        left: toggleSelect ? "0" : "-100vw",
+                        position:"absolute",
+                        backgroundColor:"red",
+                        opacity:"1"
                     }
                 } className="dashboard-overlay"
-                onClick={(e)=>{
-                e.stopPropagation()
-                setToggleSelect(false)
-            }}
+                    onClick={(e) => {
+                        e.stopPropagation()
+                        setToggleSelect(false)
+                    }}
                 >
 
-                </div>
+                </div> */}
 
             </div>
 
